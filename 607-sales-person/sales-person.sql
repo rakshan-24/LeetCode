51a8ebe0da0d0130dd1496d85ddab23ffea6d58f
rakshan-24 <rakshanfaeq@gmail.com>
2026-08-27 14:16:10 +0530

    Time: 1445 ms (75.43%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select name from SalesPerson
+where sales_id NOT IN (
+    select sales_id from Orders
+    where com_id = (
+        select com_id from Company
+        where name='RED'
+    )
+);
